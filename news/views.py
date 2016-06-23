9140f7591dd3fcc6363007d30d42a550838322cc
morreene <morreene@gmail.com>
2016-06-23 22:29:25 +0200

    lession 3.4

--- news/views.py
+++ news/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from .models import Article, Feed
 
 # Create your views here.
 
 def articles_list(request):
-	return render(request, 'news/articles_list.html', {})
+	articles = Article.objects.all()
+
+	return render(request, 'news/articles_list.html', {'articles':articles})
+
+def feeds_list(request):
+	feeds = Feed.objects.all()
+
+	return render(request, 'news/feeds_list.html', {'feeds':feeds})
+
+def new_feed(request):
+	return render(request, 'news/new_feed.html', {})
