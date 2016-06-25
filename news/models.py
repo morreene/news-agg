7f575e13d954af2c2220e6bc7ebe03e4c83cc3f2
morreene <morreene@gmail.com>
2016-06-25 08:36:53 +0200

    complet

--- news/models.py
+++ news/models.py
@@ -1,5 +1,4 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
@@ -8,11 +7,19 @@ class Feed(models.Model):
 	url = models.URLField()
 	is_active = models.BooleanField(default=False)
 
+	def __str__(self):
+		return self.title.encode('utf8')
+
+
+
 class Article(models.Model):
 	feed = models.ForeignKey(Feed)
-
 	title = models.CharField(max_length=200)
 	url = models.URLField()
 	description = models.TextField()
 	publication_date = models.DateTimeField()
+
+	def __str__(self):
+		return self.title.encode('utf8')
+
 
