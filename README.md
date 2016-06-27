88efd5d4fe5aa5c78e05519386d2e8747735313d
morreene <morreene@gmail.com>
2016-06-27 18:46:53 +0200

    page

---
+++ README.md
@@ -0,0 +1,13 @@
+
+
+
+
+
+* nltk error: 'tokenizers/punkt/english.pickle' not found.
+
+Go into a python shell and type:
+^^^
+>>> import nltk
+>>> nltk.download()
+^^^
+Then an installation window appears. Go to the 'Models' tab and select 'punkt' from under the 'Identifier' column. Then click Download and it will install the necessary files. 
--- newsproject/settings.py
+++ newsproject/settings.py
@@ -42,8 +42,31 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pagination',
     'news',
 )
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.media",
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    "django.core.context_processors.request",
+    "django.core.context_processors.i18n",
+    "django.contrib.messages.context_processors.messages",
+    "planet.context_processors.context"
+)
+
+
+
+# TEMPLATE_CONTEXT_PROCESSORS = (
+#     "django.core.context_processors.auth",
+#     "django.core.context_processors.debug",
+#     "django.core.context_processors.i18n",
+#     "django.core.context_processors.media",
+#     "django.core.context_processors.request"
+# )
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -54,6 +77,8 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+
+    'pagination.middleware.PaginationMiddleware',
 )
 
 ROOT_URLCONF = 'newsproject.urls'
@@ -106,3 +131,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
